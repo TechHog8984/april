089dc96ffec0353e9fd829728b367d9b154aa946
TechHog <techhog@example.com>
2026-08-18 11:43:48 -0400

    update rebuildskep.sh to use JAVA_HOME and not go to /tmp because lute can't handle symlinks so im just dealing with stopping my lsp

--- io/rebuildskep.sh
+++ io/rebuildskep.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rm -rf /tmp/skep/* && ../build/april --jar=/usr/lib/jvm/openjdk8/jre/lib/rt.jar --output-folder=/tmp/skep
+rm -rf skep/* && ../build/april --jar=$JAVA_HOME/jre/lib/rt.jar --output-folder=skep
